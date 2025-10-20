2780fc78e5f47561be31b97b68826932cd0812f7
ppoliani <ppoliani@gmail.com>
2025-10-20 12:28:28 +0300

    feat: add seeder

--- tfhe/src/js_on_wasm_api/js_high_level_api/mod.rs
+++ tfhe/src/js_on_wasm_api/js_high_level_api/mod.rs
@@ -9,6 +9,8 @@ pub(crate) mod keys;
 #[cfg(feature = "zk-pok")]
 mod zk;
 
+use crate::{shortint::engine::ShortintEngine, integer::oprf::Seeder, Seed};
+
 use super::into_js_error;
 
 pub(crate) fn catch_panic_result<F, R>(closure: F) -> Result<R, JsError>
@@ -25,6 +27,37 @@ where
 {
     std::panic::catch_unwind(std::panic::AssertUnwindSafe(closure))
         .map_or_else(|_| Err(JsError::new("Operation Failed")), |ret| Ok(ret))
+}
+
+pub struct DeterministicSeeder {
+  seed: Seed,
+}
+
+impl DeterministicSeeder {
+  pub fn new(seed: u128) -> Self {
+    Self {
+      seed: Seed(seed),
+    }
+  }
+}
+
+impl Seeder for DeterministicSeeder {
+    fn seed(&mut self) -> Seed {
+        self.seed
+    }
+
+    fn is_available() -> bool
+    where
+      Self: Sized {
+        true
+    }
+}
+
+#[wasm_bindgen]
+pub fn set_seeder(seed: u128) {
+    ShortintEngine::with_thread_local_mut(|engine| {
+        *engine = ShortintEngine::new_from_seeder(&mut DeterministicSeeder::new(seed))
+    });
 }
 
 #[wasm_bindgen]
